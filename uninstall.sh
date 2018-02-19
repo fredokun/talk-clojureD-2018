4abcce9f23ad656e26dcd0017df3c2ccbee2e868
fredokun <me@whereiam.now>
2018-02-19 15:10:47 +0100

    uninstall

---
+++ uninstall.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+cd html/
+
+# clone deckjs repo if required
+if [ -d "deck.js/" ]; then
+    echo "Unnstalling deck.js ..."
+    rm -rf deck.js/
+fi
+
+# also for asciidoctor-deckjs
+if [ -d "asciidoctor-deck.js/" ]; then
+    echo "Uninstalling asciidoctor-deck.js ..."
+    rm -rf asciidoctor-deck.js 
+fi
+
+
